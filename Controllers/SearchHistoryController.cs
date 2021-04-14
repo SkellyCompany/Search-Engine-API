2596d167c3a0dbda6645267feb1503d1b0dfa753
kidagine <davidkalatzis1999@hotmail.com>
2021-04-14 10:56:49 +0200

    Added search history controller.

---
+++ Controllers/SearchHistoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchEngine.API.Core.ApplicationServices;
+using System.Collections.Generic;
+
+namespace SearchEngine.API.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class SearchHistoryController : ControllerBase
+	{
+		private readonly ISearchHistoryService _searchHistoryService;
+
+		public SearchHistoryController(ISearchHistoryService searchHistoryService)
+		{
+			_searchHistoryService = searchHistoryService;
+		}
+
+		[HttpGet()]
+		public IActionResult GetHistory([FromQuery] string keyword, [FromQuery] int maxAmount)
+        {
+            IEnumerable<string> searchHistory = _searchHistoryService.GetHistory(keyword, maxAmount);
+			return Ok(searchHistory);
+		}
+
+		[HttpPost()]
+		public IActionResult SaveHistory([FromQuery] string keyword)
+		{
+			_searchHistoryService.SaveHistory(keyword);
+			return Ok();
+		}
+
+		[HttpDelete()]
+		public IActionResult DeleteHistory([FromQuery] string keyword)
+		{
+			_searchHistoryService.DeleteKeywordFromHistory(keyword);
+			return Ok();
+		}
+	}
+}
--- Core/Domain/Entity/SearchHistory.cs
+++ Core/Domain/Entity/SearchHistory.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 
 namespace SearchEngine.API.Core.Domain.Entity
 {
 	public class SearchHistory
 	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+		[BsonElement("keyword")]
 		public string Keyword { get; set; }
+		[BsonElement("dates")]
 		public List<DateTime> Dates { get; set; }
 	}
 }
